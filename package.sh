e155a983693b06f2b720c43442ac486fdfa622fe
RedTeamingSec <redteamsectorid@gmail.com>
2025-06-26 23:07:25 +0700

    Create package.sh

---
+++ package.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install -y \
+    build-essential bison gawk texinfo wget \
+    python3 git tar gzip bzip2 xz-utils \
+    m4 make patch gcc g++ libc6-dev libc6-dev \
+    libgmp-dev libmpfr-dev libmpc-dev
